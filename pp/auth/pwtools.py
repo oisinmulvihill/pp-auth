78c43caaf65675075cde303b586eb87c7e9df965
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2012-11-03 06:31:05 -0700

    updated docs and stripped unused code.

--- pp/auth/pwtools.py
+++ pp/auth/pwtools.py
@@ -2,14 +2,11 @@
 """
 This provides password hashing and validate functions which can be
 
+This uses passlib.hash sha512_crypt to implement strong passord hashing rather
+then some home brew approach.
+
 """
-import os
-import hashlib
-
 from passlib.hash import sha512_crypt
-
-
-SHA1_SALT_LENGTH = len(hashlib.sha1().hexdigest())
 
 
 def hash_password(plaintext_password):
@@ -23,22 +20,9 @@ def hash_password(plaintext_password):
     else:
         password_8bit = plaintext_password
 
-    #print "hash_password password_8bit: ", password_8bit, type(password_8bit)
-
-    # Salt is ok as sha1 but much stronger sha is going to be used for
-    # the actual password.
-    salt = hashlib.sha1()
-    salt.update(os.urandom(60))
-
-    # hashed = hashlib.sha512()
-    # hashed.update(password_8bit + salt.hexdigest())
-    # hashed_password = salt.hexdigest() + hashed.hexdigest()
-
+    # Use password lib strong hashing rather then home brew:
     hashed_password = sha512_crypt.encrypt(password_8bit)
 
-    # Make sure the hased password is an UTF-8 object at the end of the
-    # process because SQLAlchemy _wants_ a unicode object for Unicode
-    # fields
     if not isinstance(hashed_password, unicode):
         hashed_password = hashed_password.decode('UTF-8')
 
@@ -65,36 +49,4 @@ def validate_password(plaintext_password, password_hash):
     if isinstance(password_hash, unicode):
         password_hash = password_hash.encode('UTF-8')
 
-    #print "validate_password: password_8bit: ", password_8bit, type(password_8bit)
-
-    #hashed = hashlib.sha512()
-    #hashed.update(password_8bit + password_hash[:SHA1_SALT_LENGTH])
-    #return password_hash[SHA1_SALT_LENGTH:] == hashed.hexdigest()
-
     return sha512_crypt.verify(password_8bit, password_hash)
-
-
-def validate_password_hash(pass_hash_one, pass_hash_two):
-    """Checks that the first password hash equals the second.
-
-    :returns: True, password ok i.e. one == two (after salt removed).
-
-    """
-    if isinstance(pass_hash_one, unicode):
-        password_one_8bit = pass_hash_one.encode('UTF-8')
-    else:
-        password_one_8bit = pass_hash_one
-
-    if isinstance(pass_hash_two, unicode):
-        password_two_8bit = pass_hash_two.encode('UTF-8')
-    else:
-        password_two_8bit = pass_hash_two
-
-    #print "validate_password_hash: password_8bit: ", password_one_8bit, password_two_8bit
-
-    one = password_one_8bit[SHA1_SALT_LENGTH:]
-    two = password_two_8bit[SHA1_SALT_LENGTH:]
-
-    #print "Hashes minus salt Equal? %s" % (one == two)
-
-    return (one == two)
--- pp/auth/tests/test_pwtools.py
+++ pp/auth/tests/test_pwtools.py
@@ -6,9 +6,6 @@ Edward Easton, Oisin Mulvihill
 from pp.auth import pwtools
 
 
-# TODO: add tests with pre-canned results - these tests are mostly pretty meaningless
-
-
 def test_hash_then_unhash():
     plain_password = "11amcoke"
     hashed_pw = pwtools.hash_password(plain_password)
@@ -43,15 +40,3 @@ def test_hash_then_unhash_fail_unicode_other():
     plain_password = u"manÃna123"
     hashed_pw = pwtools.hash_password(plain_password)
     assert not pwtools.validate_password("not the password", hashed_pw)
-
-
-#def test_verify_hash():
-#    plain_password = u"manÃna123"
-#    hashed_pw = pwtools.hash_password(plain_password)
-#    assert pwtools.validate_password_hash(hashed_pw, pwtools.hash_password(plain_password))
-
-#    plain_password = u"manÃna123"
-#    hashed_pw = pwtools.hash_password(plain_password)
-#    assert not pwtools.validate_password_hash(
-#        pwtools.hash_password("should not validate"), hashed_pw
-#    )

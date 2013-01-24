c4791060fd8a05e69fd1efc8ebd0e8512e0f8ab8
Edward Easton <edward.easton@pythonpro.co.uk>
2013-01-24 19:57:59 +0000

    Updated to use pkglib

---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. pp.auth documentation master file
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+pp.auth
+===============================
+
+.. toctree::
+   :maxdepth: 4
+
+   autodoc/pp.auth
+
+.. automodule:: pp.auth
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
--- setup.py
+++ setup.py
@@ -1,73 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Setuptools script for pp-auth (pp.auth)
+# $HeadURL$
+try:
+    from pkglib.setuptools import setup
+except ImportError:
+    print "PkgLib is not available. Please run \"easy_install pkglib\""
+    import sys
+    sys.exit(1)
 
-"""
-from setuptools import setup, find_packages
+# ------------------ Define your C-extensions here --------------------- #
 
-Name = 'pp-auth'
-ProjectUrl = ""
-Version = "1.0.2dev"
-Author = ''
-AuthorEmail = 'everyone at pythonpro dot co dot uk'
-Maintainer = ''
-Summary = ' pp-auth '
-License = ''
-Description = Summary
-ShortDescription = Summary
+# Conventions:
+# Source code under '<package root>/src/'
+# Extension modules names begin with an underscore: eg, '_xyz'
+# to differentiate them from regular Python modules.
 
-needed = [
-    "mock",
-    'passlib',
-    "pp-db",
-    "repoze.who==1.0.19",
-    "repoze.what==1.0.9",
-    "repoze.what.plugins.ini==0.2.2",
-]
+# import numpy
+# extra_compile_args = ['-O0']
 
-test_needed = [
-]
+# setup( ext_modules = [
+#        Extension('acme.mypackage._foo', ['src/foo1.c', 'src/foo2.c']  \
+#                   include_dirs=[ numpy.get_include() ],
+#                   extra_compile_args=extra_compile_args,
+#        ),
+#        Extension('acme.mypackage._bar', ['src/bar1.c', 'src/bar2.c']  \
+#                   include_dirs=[ numpy.get_include() ],
+#                   extra_compile_args=extra_compile_args,
+#       ),
+# ])
 
-test_suite = 'pp.auth.tests'
-
-EagerResources = [
-    'pp',
-]
-
-# Example including shell script out of scripts dir
-ProjectScripts = [
-]
-
-PackageData = {
-    '': ['*.*'],
-}
-
-# Example console script and paster template integration:
-EntryPoints = {
-}
-
-
-setup(
-    url=ProjectUrl,
-    name=Name,
-    zip_safe=False,
-    version=Version,
-    author=Author,
-    author_email=AuthorEmail,
-    description=ShortDescription,
-    long_description=Description,
-    classifiers=[
-        "Programming Language :: Python",
-    ],
-    license=License,
-    scripts=ProjectScripts,
-    install_requires=needed,
-    tests_require=test_needed,
-    test_suite=test_suite,
-    include_package_data=True,
-    packages=find_packages(),
-    package_data=PackageData,
-    eager_resources=EagerResources,
-    entry_points=EntryPoints,
-    namespace_packages=['pp'],
-)
+setup()
